bc30b183135431238bb6640b20b0c0aeb84af85a
smaxxims <smaxxims@googlemail.com>
2023-01-27 00:16:58 +0100

    added profile

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Profile from '../views/Profile.vue'
+import Todo from '../views/profile/Todo.vue'
+import Post from '../views/profile/Post.vue'
 
 const routes = [
   {
@@ -14,7 +17,33 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/profile/:id',
+    component: Profile,
+    children: [
+      {
+        path: 'post',
+        component: Post,
+      },
+      {
+        path: 'todo',
+        component: Todo,
+      },
+    ],
+    beforeEnter: (to, from, next) => {
+      console.log('from.path', from.path)
+      console.log('to.path', to.path)
+      next()
+      /* 
+        if(loggedInn && to.path !== '/login') {
+          next();
+        } else {
+          next('/login');
+        }
+      */
+    },
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/Profile.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <router-link :to="`/profile/${$route.params.id}/todo`">Zu den Todos</router-link>
+    <router-link :to="`/profile/${$route.params.id}/post`">Zu den Posts</router-link>
+    <h1>Profile {{ id }}</h1>
+    <h6>{{ userData }}</h6>
+    <router-view :user="userData"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      userData: {},
+      name: '',
+    };
+  },
+  created() {
+    fetch(`https://jsonplaceholder.typicode.com/users/${this.id}`)
+    .then(response => response.json())
+    .then(response => this.userData = response)
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style>
+.router-link-active {
+  color: #e74c3c;
+}
+</style>
